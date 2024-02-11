2bcc14a9d8f689dfa80ba6d1e8a22832596d16b2
ben05allen <ben05allen@hotmail.com>
2024-02-11 20:54:14 +0900

    adding results

--- test.py
+++ test.py
@@ -1,6 +1,10 @@
+import pydantic
 from schedules import Sale
+import time   
 
 NUM_RUNS = 100_000
+
+print(pydantic.__version__)
 
 row = {
     "Date": "2023-3-14",
@@ -9,5 +13,8 @@ row = {
     "Amount": 2,
     "Unit Price": 450
 }
+
+start_time = time.time()
 for _ in range(NUM_RUNS):
     Sale(**row)
+print(time.time()-start_time)
